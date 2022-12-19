d61206a342776c9132dea44189860d9a32324884
Alhassan Mohamed <65092971+alhassanmohamed2@users.noreply.github.com>
2022-12-20 00:58:57 +0200

    Update GaussSeidelGeneral.m

--- GaussSeidelGeneral.m
+++ GaussSeidelGeneral.m
@@ -9,28 +9,48 @@ no_equ = input('Enter Number of equations \n');
 x = zeros(1, no_equ); %fixed size array
 x_old = zeros(1, no_equ); %fixed size array
 
+% make the user input every single inital values of x
 for no = 1:no_equ
     x(no)= input(['Enter inital value of X' num2str(no) '\n']);    
 end
 
-Augmented_matrix = input('Enter Augmented matrix\n');
+% check that the enterd matrix is the right one in size or not
+while(true)
+    Augmented_matrix = input('Enter Augmented matrix\n');
+    if(isequal(size(Augmented_matrix) ,[no_equ,  (no_equ+1)]))
+        break;
+    end
+    fprintf('Error!\n Write the matrix in the correct way:  %d * %d Matix\n ' ,no_equ , no_equ +1);
+end
+
 
 for i = 1:no_iter
   fprintf('==== Iteration no. %d ====\n', i)
   x_old = x;
-  for j = 1:no_equ
+  % This (for) loop is for going through every row in the matrix. 
+  for row_num = 1:no_equ
       
-      x(j) = Augmented_matrix(j, no_equ + 1);
-      for r = 1:no_equ
-         if(r ~= j)
-             x(j) = x(j) - Augmented_matrix(j, r) * x(r); 
+      % we put the value after = sign first to subtracte every other
+      % element in the vector from it
+      x(row_num) = Augmented_matrix(row_num, no_equ + 1);
+      
+      % This for loop is for going through every element in the 
+      % row for calculating the actual values we need.
+      for element_num = 1:no_equ
+          
+         if(element_num ~= row_num)
+             x(row_num) = x(row_num) - Augmented_matrix(row_num, element_num) * x(element_num); 
          end  
+         
       end
-      x(j) = x(j) / Augmented_matrix(j,j);
-      err = abs( (x(j) - x_old(j) ) / x(j) ) * 100;
+   
+      x(row_num) = x(row_num) / Augmented_matrix(row_num,row_num); %calculate the num value of x(n)
+      err = abs( (x(row_num) - x_old(row_num) ) / x(row_num) ) * 100; %calculate the error
       
-      fprintf('x%d = %4.6f \n',j,x(j))
-      fprintf('Err of x%d = %4.6f \n',j,err)
+      %print the values
+      fprintf('x%d = %4.6f \n',row_num,x(row_num))
+      fprintf('Err of x%d = %4.6f \n',row_num,err)
+      
   end
   
 end
